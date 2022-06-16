140e2fa806593fb8c44bc381896f452a0e1b2672
UnicChan <42190000+UnicChan@users.noreply.github.com>
2022-06-16 16:58:42 +0300

    a
    a

--- poof.py
+++ poof.py
@@ -4,16 +4,9 @@ from flask import send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return render_template('home.html', title = "Test")
-
-@app.route("/index")
 def index():
     return render_template('index.html', title = "index")
 
-@app.route("/tekst")
-def itekst():
-    return render_template('tekst.html', title = "tekst")
 
 if __name__=="__main__":
     app.run(debug=True)
